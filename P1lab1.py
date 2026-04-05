701e41e30ad04dd0e5848c736daa48c9337d6fbd
camaraha7289-cell <camaraha7289@student.faytechcc.edu>
2026-04-05 19:33:05 -0400

    First Python Assignment
    Taking input from user and printing output.

---
+++ P1lab1.py
@@ -0,0 +1,8 @@
+#Anthony Camara Hernandez
+#2 April 2026
+#P1LAB1
+#
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name + "!", "Welcome to CTI-110")
